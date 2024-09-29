6431328a1a5d7c5196eee74fc8cd8cd77f21f3d7
skyt2t <skyasar210@gmail.com>
2024-09-29 16:21:11 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
     try {
       console.log(formData);
       await axios
-        .post("http://localhost:4000/api/user/register", formData)
+        .post("https://smoktre-2.onrender.com/api/user/register", formData)
         .then((res) => {
           console.log(res);
           toast.success("User registered successfully");
